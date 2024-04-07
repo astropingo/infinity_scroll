d4d3c6215d7db8eb2971ad7642ae9570d1299dad
mvp <mvpetri@gmail.com>
2024-04-07 00:07:27 -0300

    Rendering images from Unsplash.

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
+// apiKey comes from the file credentials.js. It is on .gitignore for security reasons.
+// Not the best practice but whatever, I am just testing this out to learn how to do it.
+
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+let photosArray = [];
+
 const count = 10;
 let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        const item = document.createElement("a");
+        setAttributes(item, {
+            href: photo.links.html,
+            target: "_blank"
+        });
+        const img = document.createElement("img");
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
 
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos();
     } catch (error) {
-
+        console.log('Error: ', error);
     }
 }
 getPhotos();
