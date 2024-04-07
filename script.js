b1a2c4d37fa85a97d2a4d1339afb52c10777e866
mvp <mvpetri@gmail.com>
2024-04-06 23:52:21 -0300

    Added image fetch function.

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const count = 10;
 let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+
+    }
+}
+getPhotos();
